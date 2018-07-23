c2117dd7ee8e73d5cbbd76118d7eaa5fb556a52a
Jordy Nelson Samuel <jordysamuel@gmail.com>
2018-07-23 21:55:55 +0800

    more changes

--- APPLICATION/Aether/src/java/com/aether/blockchain/BlockchainHandler.java
+++ APPLICATION/Aether/src/java/com/aether/blockchain/BlockchainHandler.java
@@ -5,149 +5,131 @@
  */
 package com.aether.blockchain;
 
+import com.aether.util.RESTHandler;
 import java.net.*;
 import java.util.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import org.json.simple.*;
 import org.json.simple.parser.*;
+
 /**
  *
  * @author Jordy
  */
 public class BlockchainHandler {
-    
+
     private static HttpURLConnection con;
-    private static final String URL = "http://vm.jordysamuel.com:30301/Blockchain/?sendRaw=true";
+    private static final String URL = "http://vm.jordysamuel.com:30301/Blockchain/";
     private static final double ETHTOWEI = 1000000000000000000.0;
-    
-    public static double ethToWei(double eth){
+
+    public static double ethToWei(double eth) {
         return eth * ETHTOWEI;
     }
-    
-    public static String sendPostRequest(JSONObject body) throws MalformedURLException,
-            ProtocolException, IOException{
-        byte[] postData = body.toString().getBytes(StandardCharsets.UTF_8); 
-        try{
-            URL myurl = new URL(URL);
-            con = (HttpURLConnection) myurl.openConnection();
-            
-            con.setDoOutput(true);
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json");
-            
-            try(DataOutputStream wr = new DataOutputStream(con.getOutputStream())){
-                wr.write(postData);
-            }
-            
-            StringBuilder content;
-            
-            try(BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()))){
-                
-                String line;
-                content = new StringBuilder();
-                
-                while((line = in.readLine()) != null){
-                    content.append(line);
-                    content.append(System.lineSeparator());
-                }
-            }
-            
-            System.out.println(content.toString());
-            
-            return content.toString();
-        }finally{
-            con.disconnect();
-        }
-    }
-    
-    public static JSONObject getJSONObject(String jsonString) throws ParseException{
+
+    public static JSONObject getJSONObject(String jsonString) throws ParseException {
         JSONParser parser = new JSONParser();
         return (JSONObject) parser.parse(jsonString);
     }
-    
-    public static String sendTransaction(String from, String to, double value){
+
+    public static String sendTransaction(String from, String to, double value) {
         JSONObject body = new JSONObject();
-        body.put("jsonrpc", "2.0");
-        body.put("method", "eth_sendTransaction");
-        
-        JSONObject params = new JSONObject();
-        params.put("from", from);
-        params.put("to", to);
-        params.put("value", ethToWei(value));
-        JSONArray parameters = new JSONArray();
-        parameters.add(params);
-        body.put("params", params);
-        
-        try{
-            String result = sendPostRequest(body);
+        body.put("method", "sendTransaction");
+
+        JSONObject param = new JSONObject();
+        param.put("from", from);
+        param.put("to", to);
+        param.put("value", value);
+        body.put("param", param);
+
+        try {
+            String result = RESTHandler.sendPostRequest(URL, body);
             JSONObject jsonResult = getJSONObject(result);
             String transactionHash = (String) jsonResult.get("result");
             return transactionHash;
-        }catch(MalformedURLException e){
+        } catch (MalformedURLException e) {
             System.out.println("Malformed URL!");
-        }catch(ProtocolException f){
+        } catch (ProtocolException f) {
             System.out.println("Protocol Exception!");
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("IO Exception!");
-        }catch(ParseException e){
+        } catch (ParseException e) {
             System.out.println("Parse Exception!");
         }
-        
+
         return null;
     }
-    
-    public static String createAccount(String password){
+
+    public static String createAccount(String password) {
         JSONObject body = new JSONObject();
-        body.put("jsonrpc", "2.0");
-        body.put("method", "personal_newAccount");
-        JSONArray params = new JSONArray();
-        params.add(password);
-        body.put("params", params);
-        body.put("id", 1);
-        System.out.println(body.toString());
-        try{
-            String result = sendPostRequest(body);
+        body.put("method", "createAccount");
+        JSONObject param = new JSONObject();
+        param.put("password", password);
+        body.put("param", param);
+        try {
+            String result = RESTHandler.sendPostRequest(URL, body);
             System.out.println(result);
             JSONObject jsonResult = getJSONObject(result);
             String transactionHash = (String) jsonResult.get("result");
             return transactionHash;
-        }catch(MalformedURLException e){
+        } catch (MalformedURLException e) {
             System.out.println("Malformed URL!");
-        }catch(ProtocolException f){
+        } catch (ProtocolException f) {
             System.out.println("Protocol Exception!");
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("IO Exception!");
-        }catch(ParseException e){
+        } catch (ParseException e) {
             System.out.println("Parse Exception!");
         }
-        
+
         return null;
     }
-    
-    public static ArrayList<String> getListOfAccounts(){
-        JSONObject body = new JSONObject();
-        body.put("jsonrpc", "2.0");
-        body.put("method", "eth_accounts");
-        body.put("params", new JSONArray());
-        body.put("id", 1);
-        try{
-            String result = sendPostRequest(body);
+
+    public static ArrayList<String> getListOfAccounts() {
+        String requestURL = URL + "?method=getListOfAccounts";
+        try {
+            String result = RESTHandler.sendGetRequest(requestURL);
             JSONObject jsonResult = getJSONObject(result);
-            
+
             ArrayList accountList = (ArrayList) jsonResult.get("result");
             return accountList;
-        }catch(MalformedURLException e){
+        } catch (MalformedURLException e) {
             System.out.println("Malformed URL!");
-        }catch(ProtocolException f){
+        } catch (ProtocolException f) {
             System.out.println("Protocol Exception!");
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("IO Exception!");
-        }catch(ParseException e){
+        } catch (ParseException e) {
             System.out.println("Parse Excepion!");
         }
-        
+
+        return null;
+    }
+
+    public static String getBalance(String account) {
+        String requestURL = URL + "?method=getBalance&publickey=" + account;
+        try {
+            String result = RESTHandler.sendGetRequest(requestURL);
+            JSONObject jsonResult = getJSONObject(result);
+            String hex = (String) jsonResult.get("result");
+
+            char[] chars = hex.toCharArray();
+
+            StringBuffer hexdec = new StringBuffer();
+            for (int i = 0; i < chars.length; i++) {
+                hexdec.append(Integer.toHexString((int) chars[i]));
+            }
+            return hexdec.toString();
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL!");
+        } catch (ProtocolException f) {
+            System.out.println("Protocol Exception!");
+        } catch (IOException e) {
+            System.out.println("IO Exception!");
+        } catch (ParseException e) {
+            System.out.println("Parse Excepion!");
+        }
+
         return null;
     }
 }
