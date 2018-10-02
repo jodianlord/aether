57d0216bebe9b626fa918e6b47873a3f30f0449d
Jordy Nelson Samuel <jordysamuel@gmail.com>
2018-10-02 12:53:39 +0800

    functional visualisation

--- APPLICATION/Aether/web/js/owlvisualisation.js
+++ APPLICATION/Aether/web/js/owlvisualisation.js
@@ -1,42 +1,91 @@
 var currentStage = "Contract";
 
-$("#contractImg").click(function(){
+$("#contractImg").click(function () {
     console.log("hi");
     $.alert({
         title: 'Sample Contract Code In Solidity!',
         content: "<pre><code>pragma solidity ^ 0.4.2; <br>" +
-                    "contract KYC{<br>" +
-                        "   string public uuid;<br>" +
-                        "   string public hash;<br>" +
-                        "   bool public verified = false;<br><br>" +
+            "contract KYC{<br>" +
+            "   string public uuid;<br>" +
+            "   string public hash;<br>" +
+            "   bool public verified = false;<br><br>" +
 
-                        "   constructor(string id, string ha) public{<br>" +
-                            "       uuid = id;<br>" +
-                            "       hash = ha;<br>" +
-                        "   }<br><br>" +
-                
-                        "   function setVerified() public{<br>" +
-                                    "       verified = true;<br>" +
-                        "   }<br>"+
-                    "}</code></pre>" 
+            "   constructor(string id, string ha) public{<br>" +
+            "       uuid = id;<br>" +
+            "       hash = ha;<br>" +
+            "   }<br><br>" +
+
+            "   function setVerified() public{<br>" +
+            "       verified = true;<br>" +
+            "   }<br>" +
+            "}</code></pre>"
     });
 });
 
-$("#deployImg").click(function(){
+$("#deployImg").click(function () {
     console.log("deploying contract");
-    
+    var blockchainobj = {};
+    var blockchainParam = {};
+    blockchainobj["method"] = "deployContract";
+    blockchainParam["publicKey"] = "0x2d117903f7b2dc16abe2d6272d11d84233ff7c1f";
+    blockchainParam["password"] = "password";
+    blockchainParam["uuid"] = uuid;
+    blockchainParam["hash"] = hash;
+    blockchainobj["param"] = blockchainParam;
+    console.log(blockchainobj);
+
+    var unlockobj = {};
+    var unlockparam = {};
+    unlockobj["method"] = "unlockAccount";
+    unlockparam["publicKey"] = "0x2d117903f7b2dc16abe2d6272d11d84233ff7c1f";
+    unlockparam["password"] = "password";
+    unlockobj["param"] = unlockparam;
     $.confirm({
         title: "These are the fields you'll be using",
         content: uuid + " " + hash,
-        buttons:{
-            deploy: function(){
+        buttons: {
+            deploy: function () {
                 $.ajax({
-                    beforeSend: function(request) {
+                    beforeSend: function (request) {
                         request.setRequestHeader("X-Blockchain-Key", "513091f8-ffb1-4e52-ac6b-a5e09021ef91");
-                      },
-                });
+                    },
+                    url: "http://vm.jordysamuel.com:30301/Blockchain/",
+                    type: "POST",
+                    data: JSON.stringify(unlockobj),
+                    contentType: "application/json",
+                    success: function (response) {
+                        $.ajax({
+                            beforeSend: function (request) {
+                                request.setRequestHeader("X-Blockchain-Key", "513091f8-ffb1-4e52-ac6b-a5e09021ef91");
+                            },
+                            url: "http://vm.jordysamuel.com:30301/Blockchain/",
+                            type: "POST",
+                            data: JSON.stringify(blockchainobj),
+                            contentType: "application/json",
+                            success: function (response) {
+                                console.log(response);
+                                $.alert({
+                                    title: "Success!",
+                                    content: JSON.stringify(response)
+                                })
+                            },
+                            error: function (xhr) {
+                                $.alert({
+                                    title: "Failure!",
+                                    content: "Contract not deployed"
+                                });
+                            }
+                        });
+                    },
+                    error: function (xhr) {
+
+                    }
+
+                })
+
+
             },
-            cancel: function(){
+            cancel: function () {
                 $.alert("Cancelled!");
             }
         }
