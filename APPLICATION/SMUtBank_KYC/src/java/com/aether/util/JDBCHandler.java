7654cf9df86ed00fe6caa263c5f2726b68074a6c
Jordy Nelson Samuel <jordysamuel@gmail.com>
2018-10-22 11:18:22 +0800

    Finished JDBC Handler

--- APPLICATION/SMUtBank_KYC/src/java/com/aether/util/JDBCHandler.java
+++ APPLICATION/SMUtBank_KYC/src/java/com/aether/util/JDBCHandler.java
@@ -6,6 +6,8 @@
 package com.aether.util;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -20,36 +22,107 @@ import java.util.logging.Logger;
  * @author jordysamuel
  */
 public class JDBCHandler {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Map<String, String> queryMap = new HashMap<String, String>();
-        queryMap.put("userid", "jordysamuel");
+        queryMap.put("userid", "beebee");
         queryMap.put("password", "wheeee");
-        queryMap.put("nono", "wjeja");
+        queryMap.put("publickey", "wjeja");
         try {
-            getRecord("user", queryMap);
+            ResultSet rs = createRecord("user", queryMap);
+            while(rs.next()){
+                System.out.println(rs.getString("userid"));
+                System.out.println(rs.getString("password"));
+                System.out.println(rs.getString("publickey"));
+            }
         } catch (SQLException ex) {
             Logger.getLogger(JDBCHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static Map<String, String> getRecord(String table, Map<String, String> criteria) throws SQLException{
+
+    public static ResultSet getRecord(String table, Map<String, String> criteria) throws SQLException {
         Connection con = ConnectionManager.getConnection();
-        //Statement stmt = con.createStatement();
-        
-        int questionCount = 1;
-        
-        String query = "SELECT * FROM ? WHERE ";
+        int questionCount = 0;
+
+        String query = "SELECT * FROM " + table + " WHERE ";
+
+        if (criteria == null || criteria.size() == 0) {
+            query = "SELECT * FROM " + table;
+            //System.out.println(query);
+            PreparedStatement pstmt = con.prepareStatement(query);
+            
+            System.out.println(pstmt.toString());
+
+            ResultSet rs = pstmt.executeQuery();
+            con.close();
+            return rs;
+        }
+
         Iterator it = criteria.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry pair =(Map.Entry)it.next();
-            query += pair.getKey(); 
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            query += pair.getKey();
             query += " = ?";
-            if(it.hasNext()){
+            if (it.hasNext()) {
                 query += " AND ";
             }
         }
-        System.out.println(query);
+        //System.out.println(query);
+        PreparedStatement pstmt = con.prepareStatement(query);
+
+        it = criteria.entrySet().iterator();
+        while (it.hasNext()) {
+            questionCount++;
+            Map.Entry pair = (Map.Entry) it.next();
+            pstmt.setString(questionCount, (String) pair.getValue());
+        }
+        
+        ResultSet rs = pstmt.executeQuery();
+        con.close();
+        return rs;
+    }
+
+    public static ResultSet createRecord(String table, Map<String, String> data) throws SQLException{
+        Connection con = ConnectionManager.getConnection();
+        
+        String query = "INSERT INTO " + table + "(";
+        
+        Iterator it = data.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry pair = (Map.Entry) it.next();
+            String key = (String) pair.getKey();
+            query += key;
+            if(it.hasNext()){
+                query += ",";
+            }else{
+                query += ")";
+            }
+        }
+        
+        query += " VALUES (";
+        
+        for(int i = 0; i < data.size(); i++){
+            query += "?";
+            if(i < data.size() - 1){
+                query += ",";
+            }else{
+                query += ")";
+            }
+        }
+        
+        PreparedStatement pstmt = con.prepareStatement(query);
+        
+        it = data.entrySet().iterator();
+        int qCount = 1;
+        while(it.hasNext()){
+            Map.Entry pair = (Map.Entry) it.next();
+            String value = (String) pair.getValue();
+            pstmt.setString(qCount, value);
+            qCount++;
+        }
+        
+        pstmt.executeUpdate();
+        
         return null;
     }
 }
